59ea1c72398a0fd14bf93056f5d74ffa03f3dfc0
Stangli Adadurov <easybbs@gmail.com>
2024-04-19 16:57:39 +0300

    test_login

---
+++ fixture/application.py
@@ -0,0 +1,47 @@
+# from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium import webdriver
+from fixture.session import SessionHelper
+# from fixture.group import GroupHelper
+# from fixture.contact import ContactHelper
+
+
+class Application:
+
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
+        self.wd.maximize_window()
+        # совсем без ожидания нельзя, тесты не проходят
+        self.wd.implicitly_wait(1)
+        self.session = SessionHelper(self)
+        self.base_url = base_url
+
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
+
+    def open_home_page(self):
+        wd = self.wd
+        wd.get(self.base_url)
+
+    def return_to_main_page(self):
+        wd = self.wd
+        wd.find_element_by_link_text("home page").click()
+
+    def fill_field_by_name(self, name, text):
+        wd = self.wd
+        if text is not None:
+            wd.find_element_by_name(name).clear()
+            wd.find_element_by_name(name).send_keys(text)
+
+    def destroy(self):
+        self.wd.quit()
---
+++ fixture/session.py
@@ -0,0 +1,43 @@
+import time
+
+
+class SessionHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def login(self, username, password):
+        wd = self.app.wd
+        self.app.open_home_page()
+        self.app.fill_field_by_name("username", username)
+        self.app.fill_field_by_name("password", password)
+        wd.find_element_by_css_selector("input[type='submit']").click()
+
+    def ensure_login(self, username, password):
+        if self.is_logged_in():
+            if self.is_logged_in_as(username):
+                return
+            else:
+                self.logout()
+        self.login(username, password)
+
+    def logout(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("Logout").click()
+        ''' без задержки не происходит выход из  системы '''
+        time.sleep(2)
+
+    def ensure_logout(self):
+        if self.is_logged_in():
+            self.logout()
+
+    def is_logged_in(self):
+        wd = self.app.wd
+        return len(wd.find_elements_by_link_text("Logout")) > 0
+
+    def is_logged_in_as(self, username):
+        return self.get_logged_user() == username
+
+    def get_logged_user(self):
+        wd = self.app.wd
+        return wd.find_element_by_css_selector("td.login-info-left span").text
